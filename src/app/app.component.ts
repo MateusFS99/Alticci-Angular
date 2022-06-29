f0ae13191f2cb88e3dfd00c73d5c5aec060b54dc
MateusFS99 <mateusfsantos.pfs@gmail.com>
2022-06-29 17:44:47 -0300

    - Web Page Styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AppService } from './app.service';
 
